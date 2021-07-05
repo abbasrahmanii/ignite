640e0fe0208b6d6216932d8703c61f7fd3dcb16c
abbasrahmanii <absrn26@gmail.com>
2021-07-05 21:10:16 +0430

    4. Action Creators, Redux and Thunk

--- src/reducers/gamesReducer.js
+++ src/reducers/gamesReducer.js
@@ -8,10 +8,18 @@ const initState = {
 const gamesReducer = (state=initState,action) => {
     switch(action.type) {
         case "FETCH_GAMES":
-            return {...state};
+            return {...state, ...action.payload};
         default :
             return {...state};
     }
+};
+
+//ACTION CREATOR
+const fetchGames = (userData) => {
+    return {
+        type: "FETCH_GAMES",
+        payload: userData,
+    };
 };
 
 export default gamesReducer;
